b32005bc37727e0d122571007cec88b8b3206133
danielastanus <danna_alinna@yahoo.com>
2021-10-24 21:29:44 +0300

    Tema introducere in Java

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,42 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!!!");
+        System.out.println("Hello");
+        System.out.println("Daniela");
+
+        short myFirstNumber = 2;
+        short mySecondNumber = 9;
+        int myInt = myFirstNumber + mySecondNumber;
+        System.out.println(myInt);
+
+        float numberOne = 7;
+        float numberTwo = 3;
+        float myFloat = numberOne / numberTwo;
+        System.out.println(myFloat);
+
+        short noOne = -5;
+        short noTwo = 8;
+        short noThree = 6;
+        int mySecondInt = noOne + (noTwo * noThree);
+        System.out.println(mySecondInt);
+
+        int x = 55;
+        int y =9;
+        int modulus = (x + y) % y;
+        System.out.println(modulus);
+
+        float first = 20;
+        float second = -3;
+        float third = 5;
+        float result = first + second * third / 8;
+        System.out.println(result);
+
+        short numberrOne = 5;
+        short numberrTwo = 3;
+         int finalResult = numberrOne + (15 / 3 * 2) - 8 % numberrTwo;
+        System.out.println(finalResult);
+
+
 
 
 
